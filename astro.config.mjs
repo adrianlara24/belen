4c83d25acab185f7b9e8aa23dc2f4ff753b93b74
adrianlara24 <adrianlara24@gmail.com>
2022-08-12 21:05:33 -0300

    update astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: ' https://adrianlara24.github.io',
+  base: '/belen'
 });
